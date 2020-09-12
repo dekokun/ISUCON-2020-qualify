2761fd4bb728108826135c882c3660ea8c5032d2
Krouton <krouton@hatena.ne.jp>
2020-09-12 16:23:15 +0900

    sorted

--- webapp/rust/src/main.rs
+++ webapp/rust/src/main.rs
@@ -959,12 +959,17 @@ async fn search_recommended_estate_with_chair(
             let w = chair.width;
             let h = chair.height;
             let d = chair.depth;
-            use std::cmp::max;
-            let m = max(w, max(h, d));
-            let query = "select * from estate where (door_width >= ? and door_height >= ?) order by popularity desc, id asc limit ?";
+            let mut c = vec![w, h, d];
+            c.sort();
+            let first = c[0];
+            let second = c[1];
+            let third = c[2];
+            let query = "select * from estate where(door_width >= ? and door_height >= ?) or (door_width >= ? and door_height >= ?) or order by popularity desc, id asc limit ?";
             let params: Vec<mysql::Value> = vec![
-                m.into(),
-                m.into(),
+                third.into(),
+                first.into(),
+                first.into(),
+                second.into(),
                 LIMIT.into(),
             ];
             Ok(Some(conn.exec(query, params)?))
